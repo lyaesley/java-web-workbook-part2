b8aea24342c385bca87335a4ec9e3a604bf8affb
lyae <lyaesley@gmail.com>
2023-02-20 00:26:21 +0900

    2-2장 완료

--- src/test/java/com/lyae/workbook/workbook/dao/ConnectTests.java
+++ src/test/java/com/lyae/workbook/workbook/dao/ConnectTests.java
@@ -1,9 +1,51 @@
 package com.lyae.workbook.workbook.dao;
 
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class ConnectTests {
+
+    @Test void testHikariCP() throws SQLException {
+
+        HikariConfig config = new HikariConfig();
+        config.setDriverClassName("org.mariadb.jdbc.Driver");
+        config.setJdbcUrl("jdbc:mariadb://localhost:3306/webdb");
+        config.setUsername("webuser");
+        config.setPassword("@zheld86");
+        config.addDataSourceProperty("cachePrepStmts", true);
+        config.addDataSourceProperty("prepStmtCacheSize", "250");
+        config.addDataSourceProperty("prepStmtCacheSqlLimit", "2048");
+
+        HikariDataSource ds = new HikariDataSource(config);
+        Connection connection = ds.getConnection();
+
+        System.out.println(connection);
+
+        connection.close();
+
+
+    }
+
+    @Test void testConnection() throws Exception{
+
+        Class.forName("org.mariadb.jdbc.Driver");
+
+        Connection connection = DriverManager.getConnection(
+                "jdbc:mariadb://localhost:3306/webdb",
+                "webuser",
+                "@zheld86");
+
+        Assertions.assertNotNull(connection);
+
+        connection.close();
+
+    }
 
     @Test
     public void test1() {
---
+++ src/test/java/com/lyae/workbook/workbook/dao/TodoDAOTests.java
@@ -0,0 +1,61 @@
+package com.lyae.workbook.workbook.dao;
+
+import com.lyae.workbook.jdbcex.dao.TodoDAO;
+import com.lyae.workbook.jdbcex.domain.TodoVO;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
+import java.util.List;
+
+public class TodoDAOTests {
+
+    private TodoDAO todoDAO;
+
+    @BeforeEach
+    public void ready() {
+        todoDAO = new TodoDAO();
+    }
+
+    @Test void testTime() {
+        System.out.println(todoDAO.getTime());
+    }
+
+    @Test void testInsert() throws Exception {
+        TodoVO todoVO = TodoVO.builder()
+                .title("sample title...")
+                .dueDate(LocalDate.of(2021,12,31))
+                .build();
+
+        todoDAO.insert(todoVO);
+    }
+
+    @Test void testList() throws Exception {
+
+        List<TodoVO> list = todoDAO.selectAll();
+
+        list.forEach(System.out::println);
+    }
+
+    @Test void testSelectOne() throws Exception {
+        Long tno = 1L;
+
+        TodoVO vo = todoDAO.selectOne(tno);
+
+        System.out.println(vo);
+    }
+
+    @Test void testUpdateOne() throws Exception {
+        TodoVO todoVO = TodoVO.builder()
+                .tno(1L)
+                .title("update title...")
+                .dueDate(LocalDate.of(2023, 2,20))
+                .finished(true)
+                .build();
+
+
+        todoDAO.updateOne(todoVO);
+
+
+    }
+}
